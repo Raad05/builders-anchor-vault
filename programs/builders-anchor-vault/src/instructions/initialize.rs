8637a42d67110ac64f3964a7a092f9b682553a2d
Raad05 <yamin.raad6109@gmail.com>
2026-05-12 15:08:50 +0600

    initialize vault account

--- programs/builders-anchor-vault/src/instructions/initialize.rs
+++ programs/builders-anchor-vault/src/instructions/initialize.rs
@@ -1,9 +1,39 @@
+use crate::state::VaultState;
 use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    // the user is the signer of this transaction, so we use the `Signer` type here
+    #[account(mut)]
+    pub user: Signer<'info>,
 
-pub fn handler(ctx: Context<Initialize>) -> Result<()> {
-    msg!("Greetings from: {:?}", ctx.program_id);
-    Ok(())
+    // the state account is a PDA that will hold the vault's state, so we use the `init` attribute here to create it
+    #[account(
+        init,
+        payer = user,
+        seeds = [b"state", user.key().as_ref()],
+        bump,
+        space = 64 + VaultState::INIT_SPACE
+    )]
+    pub vault_state: Account<'info, VaultState>,
+
+    // the vault account is a system account that will hold the funds, so we don't use the `init` attribute here
+    #[account(
+        seeds = [b"vault", vault_state.key().as_ref()],
+        bump
+    )]
+    pub vault: SystemAccount<'info>,
+
+    // the system program is required to create accounts (9-15 lines of code)
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Initialize<'info> {
+    pub fn initialize(&mut self, bumps: &InitializeBumps) -> Result<()> {
+        // save the data to state or state account
+        self.vault_state.vault_bump = bumps.vault;
+        self.vault_state.state_bump = bumps.vault_state;
+
+        Ok(())
+    }
 }
